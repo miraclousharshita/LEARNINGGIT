b03627710fe2f94f0659bf6682cb5af738823176
Harshita <harshita.21ug1034@iiitranchi.ac.in>
2023-01-05 19:43:54 +0530

    added message in exception.py

--- exception.py
+++ exception.py
@@ -1,6 +1,9 @@
 try:
   print(x)
 except NameError:
-  print("Variable x is not defined")
+  print(" Variable x is not defined")
 except:
   print("Something else went wrong")
+finally:
+  print("The 'try except' is finished")
+ 
